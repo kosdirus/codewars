12d89a173c7b5486a3e5a556ea16891d268478c6
kosdirus <kos.dirus@gmail.com>
2022-03-31 20:44:37 +0200

    7kyu: Growth of a Population

---
+++ 7-kyu/Growth of a Population/years_num.go
@@ -0,0 +1,10 @@
+package kata
+
+func NbYear(p0 int, percent float64, aug int, p int) int {
+	var res int
+	for p0 < p {
+		p0 += int(float64(p0)*percent/100 + float64(aug))
+		res++
+	}
+	return res
+}
